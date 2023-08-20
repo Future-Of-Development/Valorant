bc1de349f0cde3ae4e475934cfeecd79990ce1da
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2023-08-19 21:24:46 -0300

    ajuste de layout

--- src/components/menu/menuBar.vue
+++ src/components/menu/menuBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="h-32 flex items-center justify-between px-5 font-extrabold text-white">
-    <div class="border-l-2 px-5 text-5xl">
+    <div class="border-l-2 px-5 ">
       <img src="./../../assets/logos/LobbyForge.png" alt="">
     </div>
-    <div class="">
+    <div class="text-2xl px-5">
       <i class="fa-solid fa-right-to-bracket"></i>
     </div>
   </div>
