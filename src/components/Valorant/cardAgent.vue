6c317cb630f5fa763970f6de17fe8c501f90625c
Sérgio Henrique <sergohenrique663@gmail.com>
2023-08-19 21:13:16 -0300

    Card

--- src/components/Valorant/cardAgent.vue
+++ src/components/Valorant/cardAgent.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="py-5 flex flex-col justify-center items-center relative">
-    <div class="absolute inset-21 z-10 w-72 h-72 flex justify-center">
-      <img src="https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/background.png" alt="Background">
+    <div class="absolute inset-21 z-10 w-80 h-80 flex justify-center">
+      <img src="./../../assets/img/devon-fay-haven-05_r 2.png" alt="Background">
     </div>
-    <div class="w-72 h-72 z-20 flex justify-center items-center">
+    <div class="w-96 h-96 z-20 flex justify-center items-center">
       <img src="https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/fullportrait.png" alt="Gekko">
     </div>
     <div class="text-white font-bold font-valorant">
